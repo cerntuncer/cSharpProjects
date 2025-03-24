086408af980b120a27b16a0f0d155a9f213e85f5
cerntuncer <cernttuncer@gmail.com>
2025-03-24 18:12:07 +0300

    Initial commit

---
+++ temelUygulamalar/temelUygulamalar/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace temelUygulamalar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            Console.WriteLine("Bir sayı gir:");//konsola yazı yazdırmak için kullanılır
+            number = Convert.ToInt32(Console.ReadLine());//konsoldan kullanıcının yazdığı veriyi okur ama bu değer string bu yüzden tam sayıya çeviriyoruz
+            Console.WriteLine("Girilen sayı:{0}",number);
+            Console.ReadLine();//programın hemen kapanmaması için bekleme yapar.
+        }
+       
+    }
+}
