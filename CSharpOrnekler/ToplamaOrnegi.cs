53f052982b062f8c6ae3514fdab1940a028d6e84
cerntuncer <cernttuncer@gmail.com>
2025-04-01 19:10:36 +0300

    yeni bir örnek eklendi

---
+++ CSharpOrnekler/ToplamaOrnegi.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpOrnekler
+{
+    public class ToplamaOrnegi
+    {
+        public static void OrnekCalistir()
+        {
+            Console.Write("Birinci sayı: ");
+            int sayi1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("İkinci sayı: ");
+            int sayi2 = Convert.ToInt32(Console.ReadLine());
+
+            int toplam = sayi1 + sayi2;
+            Console.WriteLine($"Toplam: {toplam}");//$ koyarsak stringin içinde değişken olacak,güncel ve okunabilir yöntem
+        }
+    }
+}
