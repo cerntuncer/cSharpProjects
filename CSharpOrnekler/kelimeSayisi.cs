c32538d0551c8b20c94960c3205be7ebc0b42272
cerntuncer <cernttuncer@gmail.com>
2025-04-01 20:57:46 +0300

    yeni örnek eklendi

---
+++ CSharpOrnekler/kelimeSayisi.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpOrnekler
+{
+    public class kelimeSayisi
+    {
+        public static void kelimeSayisiBul()
+        {
+            string sentence;
+            Console.WriteLine("Stringi gir:");
+            sentence = Console.ReadLine();
+            string[] words=sentence.Split(' ');
+            Console.WriteLine("Kelime sayısı:" + words.Length);
+            Console.ReadKey();
+        }
+    }
+}
