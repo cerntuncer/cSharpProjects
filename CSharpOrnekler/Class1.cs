6e2a4b309dd8deb82e6fb18448cefee3bbb603af
cerntuncer <cernttuncer@gmail.com>
2025-04-01 19:10:36 +0300

    yeni bir örnek eklendi

---
+++ CSharpOrnekler/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpOrnekler
+{
+    public class CarpmaOrnegi
+    {
+       public static void SonucCalıstır()
+        {
+            float number1, number2, product;
+            Console.Write("Sayı-1:");
+            number1=Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("Sayı-2:");
+            number2= Convert.ToSingle(Console.ReadLine());
+            product = number1 * number2;
+            Console.WriteLine($"Sonuç: {product}");
+            Console.Read();
+        }
+
+    }
+}
--- CSharpOrnekler/Program.cs
+++ CSharpOrnekler/Program.cs
@@ -10,6 +10,8 @@ namespace CSharpOrnekler
     {
         static void Main(string[] args)
         {
+            CarpmaOrnegi.SonucCalıstır();
+
         }
     }
 }
