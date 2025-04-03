cd46eabc1f778b36773403bdb91fb3c0b49bc22d
cerntuncer <cernttuncer@gmail.com>
2025-04-03 12:11:47 +0300

    ArrayList ile ilgili örnek eklendi

---
+++ CSharpOrnekler/ArrayListOrnek2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpOrnekler
+{
+    public class ArrayListOrnek2
+    {
+        public static void ArrayList2()
+        {
+            ArrayList list = new ArrayList();//ArrayList in namespace i System.Collections eklenmeli
+            list.Add(1);
+            list.Add(2);
+            list.Add(7);
+            int toplam = 0;
+            for (int i = 0; i < list.Count; i++)
+            {
+                toplam += (int)list[i];
+            }
+            Console.WriteLine(toplam);
+            Console.ReadLine();
+        }
+    }
+}
