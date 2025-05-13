4d0760f608ce55a657b3fa8d5c3fdc5f7a7f427e
Ceren Tuncer <cernttuncer@gmail.com>
2025-05-13 11:41:21 +0300

    Temel kalıtım örneği eklendi

---
+++ KalitimOrnekleri/TemelOrnek/TemelOrnek.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemelOrnek
+{
+    public class Tasit
+    {
+        public int TekerlekSayisi;
+    }
+
+    public class Otomobil : Tasit
+    {
+        public Otomobil()
+        {
+            TekerlekSayisi = 4;
+        }
+    }
+
+    public class Motorsiklet : Tasit
+    {
+        public Motorsiklet()
+        {
+            TekerlekSayisi = 2;
+        }
+    }
+}
+
