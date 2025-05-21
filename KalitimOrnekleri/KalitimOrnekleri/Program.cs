30eda07f78852f538a4967bf3eff03bdee2d437d
Ceren Tuncer <cernttuncer@gmail.com>
2025-05-21 16:54:00 +0300

    kalıtım ile ilgili temel örnek eklendi

--- KalitimOrnekleri/KalitimOrnekleri/Program.cs
+++ KalitimOrnekleri/KalitimOrnekleri/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿
+using KalitimOrnekleri;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +12,12 @@ namespace KalitimOrnekleri
     {
         static void Main(string[] args)
         {
+            Kedi k = new Kedi();
+            k.Ad = "Minnak";
+            Console.WriteLine($"Kedinin adı: {k.Ad}");
+            k.SesCikar();   // Base sınıftan gelen metot
+            k.Miyavla();    // Kedi sınıfına özgü metot
+
         }
     }
 }
---
+++ KalitimOrnekleri/KalitimOrnekleri/TemelOrnek.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+    namespace KalitimOrnekleri
+    {
+        // Base (üst) sınıf
+        class Hayvan
+        {
+            public string Ad;
+
+            public void SesCikar()
+            {
+                Console.WriteLine("Hayvan ses çıkarıyor.");
+            }
+        }
+
+        // Derived (türemiş) sınıf
+        class Kedi : Hayvan
+        {
+            public void Miyavla()
+            {
+                Console.WriteLine("Kedi miyavlıyor.");
+            }
+        }
+
+      
+    
+}
